b258233fc5afc97d40064813d1b88c60de895587
borjasolerr <borjasolrey@gmail.com>
2019-04-15 16:40:05 +0200

    Added auth folder and config folder

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 
+const configureMiddleware = require('./middleware.js');
+const usersRouter = require('../users/usersRouter.js');
+const authRouter = require('../auth/authRouter.js');
+
 const server = express();
 
-server.use(express.json());
+configureMiddleware(server);
+
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 module.exports = server;
