2ecd2180de94edad1bdb0b529b2d63a434edcb1e
borjasolerr <borjasolrey@gmail.com>
2019-04-16 16:44:05 +0200

    Create users migrations, test registration and login

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ const server = express();
 
 configureMiddleware(server);
 
-server.use('/api/users', usersRouter);
 server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
 
 module.exports = server;
