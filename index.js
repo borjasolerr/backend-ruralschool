3df8686ea8f3080013fe2a734c3060658fe94c82
borjasolerr <borjasolrey@gmail.com>
2019-04-16 17:16:45 +0200

    set up database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 server.get('/', (req, res) => {
-  res.send('Code me, Disney!');
+  res.send('coded!');
 });
 
 const port = process.env.PORT || 4000;
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,11 @@
+const localPgConnection = {
+  host: 'localhost',
+  database: 'users',
+  user: 'Borja',
+  password: 'a1s2d3f4g5'
+};
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -18,6 +26,16 @@ module.exports = {
       filename: './database/test.db3'
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations'
+    },
+    seeds: {
+      directory: './database/seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: prodDbConnection,
     migrations: {
       directory: './database/migrations'
     },
